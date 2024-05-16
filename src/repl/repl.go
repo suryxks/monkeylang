ee409b36eccfdba74c502d426bf38754cb1de025
suryxks <suryasuresh9876543@gmail.com>
2024-05-16 15:52:23 +0530

    add a basic repl which prints out the tokens for now

---
+++ src/repl/repl.go
@@ -0,0 +1,28 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkeylang/src/lexer"
+	"monkeylang/src/token"
+)
+
+const PROMT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+
+}
