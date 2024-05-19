244e4a7d62b90c9b8d21b08594f8d7dde1791d16
suryxks <suryasuresh9876543@gmail.com>
2024-05-19 15:36:33 +0530

    feat: convert ast to string

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkeylang/src/token"
+import (
+	"bytes"
+	"monkeylang/src/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 type Statement interface {
 	Node
@@ -29,7 +33,12 @@ type Identifier struct {
 }
 
 type ReturnStatement struct {
-	Token token.Token
+	Token       token.Token
+	ReturnValue Expression
+}
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
 }
 
 func (p *Program) TokenLiteral() string {
@@ -45,10 +54,57 @@ func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
-func (i *Identifier) expressionNode()            {}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (i *Identifier) ExpressionNode() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+func (i *Identifier) String() string {
+	return i.Value
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
